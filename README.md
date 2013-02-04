323a55a5b9e31c56751facf2fbc17c59011b707a
Antonio Villamarin <ant@onio.org>
2013-02-04 09:06:25 +0100

    Add examples

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-notify.jquery
-=============
+# JQuery Notify v1.0
 
-Message notify on web top with jQuery.
+## Message notify on web top with jQuery.
+
+### Usage
+
+Put your message on html file.
+
+~~~
+	<div id="anyIDtag">Information/Deal/Alert message</div>
+~~~
+
+Include lib.
+
+~~~  
+	<script src="notify.jquery.min.js"></script>
+~~~
+
+Call plugin with styles.
+
+~~~
+	<script>
+		$(document).ready(function(){
+			$('#anyIDtag').notify({ // Optional parameters. Default values.
+				active: true; // True: Show message, False: Show symbol to show message.
+				close: false; // True: User click on close message hide, False: User click on close show arrow to show message again.
+				color: "#888"; // Font Color box
+				background: "#FF9"; // Background color box
+				border: "#FFF"; // Bottom border Color box
+				opacity: ".9" // Opacity box
+			});
+		});
+	</script>
+~~~
+
+Or without slyles.
+
+~~~
+	<script>
+		$(document).ready(function(){
+			$('#anyIDtag').notify(); // Use default style values.
+		});
+	</script>
+~~~
+ 
+(c) 2013, [Antonio Villamarin][1]
+ 
+License: MIT [http://opensource.org/licenses/MIT][2]
+ 
+[1]: http://ant.onio.org
+[2]: http://opensource.org/licenses/MIT
--- src/notify.jquery.js
+++ src/notify.jquery.js
@@ -1,4 +1,41 @@
-// JQuery Notify by Antonio Villamarin <http://ant.onio.org> : MIT
+/***
+ *
+ *  JQuery Notify v1.0
+ * 	Message notify with jQuery easily.
+ * 
+ * 	(c) 2013, Antonio Villamarin <http://ant.onio.org>
+ *
+ * 	License: MIT <http://opensource.org/licenses/MIT>
+ * 
+ * 	Use:
+ * 		<div id="anyIDtag">Information/Deal/Alert message</div>
+ * 
+ * 		[...]
+ * 
+ * 		<script>
+ * 			$(document).ready(function(){
+ * 				$('#anyIDtag').notify({ // Optional parameters. Default values.
+ * 					active: true; // True: Show message, False: Show symbol to show message.
+ * 					close: false; // True: User click on close message hide, False: User click on close show arrow to show message again.
+ * 					color: "#888"; // Font Color box
+ * 					background: "#FF9"; // Background color box
+ * 					border: "#FFF"; // Bottom border Color box
+ * 					opacity: ".9" // Opacity box
+ * 				});
+ *			});
+ * 		</script>
+ * 
+ * 		[...Maybe...]
+ * 
+ * 		<script>
+ * 			$(document).ready(function(){
+ * 				$('#anyIDtag').notify();
+ *			});
+ * 		</script>
+ * 
+ *
+ ******************************/
+ 
 (function(e){
 	jQuery.fn.extend({
 		notify:function(options){
@@ -7,6 +44,7 @@
 				close: false,
 				color: "#888",
 				background: "#FF9",
+				border: "#FFF",
 				opacity: ".9"
 			}, options);
 			
@@ -24,30 +62,35 @@
 				$('head').append("<style>.notify-arrow:before{display:block;font-size:20px;padding:5px 15px;content:'\\25BE';color:"+this.options.color+";}"+".notify-close:before{display:block;font-size:20px;float:right;margin:-7px -5px -7px 20px;content:'\\25B4';color:"+this.options.color+";}<\/style>");
 			}
 
-			$('.notify-message')
-				.css('background-color',this.options.background)
-				.css('z-index','101')
-				.css('position','absolute')
-				.css('top','0')
-				.css('right','0')
-				.css('left','0')
-				.css('font-weight','bold')
-				.css('font-size','16px' )
-				.css('padding','10px 20px')
-				.css('overflow','hidden')
-				.css('box-shadow','0 0 5px #444')
-				.css('opacity',this.options.opacity);
+			$('.notify-message').css({
+				'background-color': this.options.background,
+				'z-index': '101',
+				'position': 'absolute',
+				'top': '0',
+				'right': '0',
+				'left': '0',
+				'font-weight': 'bold',
+				'font-size': '16px' ,
+				'padding': '10px 20px',
+				'overflow': 'hidden',
+				'box-shadow': '0 0 5px #444',
+				'border': '0',
+				'border-bottom': '2px solid '+this.options.border,
+				'opacity': this.options.opacity
+			});
 
-			$('.notify-arrow')
-				.css('background-color',this.options.background)
-				.css('z-index','101')
-				.css('position','absolute')
-				.css('top','0')
-				.css('right','50px')
-				.css('overflow','hidden')
-				.css('box-shadow','0 0 5px #444')
-				.css('opacity',this.options.opacity)
-				.css('border-radius','0 0 5px 5px');
+			$('.notify-arrow').css({
+				'background-color': this.options.background,
+				'z-index': '101',
+				'position': 'absolute',
+				'top': '0',
+				'right': '50px',
+				'overflow': 'hidden',
+				'box-shadow': '0 0 5px #444',
+				'opacity': this.options.opacity,
+				'border': '0 2px 2px 2px solid '+this.options.border,
+				'border-radius': '0 0 5px 5px'
+			});
 			
 			if( this.options.active ) {
 				$('.notify-message').show();
